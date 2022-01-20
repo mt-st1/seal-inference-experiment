6620c296d57b56750ab2be4d31425633571b6bd1
Takumi Ishiyama <mt.stone20@gmail.com>
2022-01-20 21:35:32 +0900

    Fix scripts a bit

--- cifar-10-calculate-homOps.sh
+++ cifar-10-calculate-homOps.sh
@@ -6,48 +6,48 @@ commands=(
   # Batch-axis
   ##############
   # CIFAR-10: Square [Level: 8]
-  # "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L8_50-30 -D cifar-10 -M 5layer_cnn-square-BN --model-params 5layer_cnn-square-BN-81.42_200epoch-1225_0027-params.h5 -A square --fuse-layer --opt-pool --mode batch -N 1)"
+  # "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L8_50-30 -D cifar-10 -M 5layer_cnn-square-BN --model-params 5layer_cnn-square-BN-81.42_200epoch-1225_0027-params.h5 -A square --fuse-layer --opt-pool --mode batch -N 1)"
   # CIFAR-10: Swish approx. (rg5_deg2, rg7_deg4) [Level: 8 or 11]
-  # "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L8_50-30 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN --model-params 5layer_cnn-swish_rg5_deg2-BN-81.84_200epoch-1225_1046-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
-  # "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L11_50-30 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN --model-params 5layer_cnn-swish_rg7_deg4-BN-83.81_200epoch-1225_1703-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
+  # "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L8_50-30 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN --model-params 5layer_cnn-swish_rg5_deg2-BN-81.84_200epoch-1225_1046-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
+  # "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L11_50-30 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN --model-params 5layer_cnn-swish_rg7_deg4-BN-83.81_200epoch-1225_1703-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
 
   # CIFAR-10(GAP): Square [Level: 7]
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L7_50-30 -D cifar-10 -M 5layer_cnn-square-BN-GAP --model-params 5layer_cnn-square-BN-GAP-79.17_200epoch-0106_1855-params.h5 -A square --fuse-layer --opt-pool --mode batch -N 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L7_50-30 -D cifar-10 -M 5layer_cnn-square-BN-GAP --model-params 5layer_cnn-square-BN-GAP-79.17_200epoch-0106_1855-params.h5 -A square --fuse-layer --opt-pool --mode batch -N 1)"
   # CIFAR-10(GAP): Swish approx. (rg5_deg2, rg7_deg4) [Level: 7 or 10]
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L7_50-30 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN-GAP --model-params 5layer_cnn-swish_rg5_deg2-BN-GAP-79.53_200epoch-0107_0014-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L10_50-30 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN-GAP --model-params 5layer_cnn-swish_rg7_deg4-BN-GAP-80.91_200epoch-0107_0317-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L7_50-30 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN-GAP --model-params 5layer_cnn-swish_rg5_deg2-BN-GAP-79.53_200epoch-0107_0014-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L10_50-30 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN-GAP --model-params 5layer_cnn-swish_rg7_deg4-BN-GAP-80.91_200epoch-0107_0317-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
 
   ##############
   # Batch-axis (Pruned model)
   ##############
   # CIFAR-10: Square [Level: 8]
-  # "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L8_50-30 -D cifar-10 -M 5layer_cnn-square-BN --model-structure 5layer_cnn-square-BN-round1-65.68-structure.json --model-params 5layer_cnn-square-BN-round1-65.68-params.h5 -A square --fuse-layer --opt-pool --mode batch -N 1)"
+  # "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L8_50-30 -D cifar-10 -M 5layer_cnn-square-BN --model-structure 5layer_cnn-square-BN-round1-65.68-structure.json --model-params 5layer_cnn-square-BN-round1-65.68-params.h5 -A square --fuse-layer --opt-pool --mode batch -N 1)"
   # CIFAR-10: Swish approx. (rg5_deg2, rg7_deg4) [Level: 8 or 11]
-  # "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L8_50-30 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN --model-structure 5layer_cnn-swish_rg5_deg2-BN-round1-73.18-structure.json --model-params 5layer_cnn-swish_rg5_deg2-BN-round1-73.18-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
-  # "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L11_50-30 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN --model-structure 5layer_cnn-swish_rg7_deg4-BN-round1-75.07-structure.json --model-params 5layer_cnn-swish_rg7_deg4-BN-round1-75.07-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
+  # "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L8_50-30 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN --model-structure 5layer_cnn-swish_rg5_deg2-BN-round1-73.18-structure.json --model-params 5layer_cnn-swish_rg5_deg2-BN-round1-73.18-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
+  # "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L11_50-30 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN --model-structure 5layer_cnn-swish_rg7_deg4-BN-round1-75.07-structure.json --model-params 5layer_cnn-swish_rg7_deg4-BN-round1-75.07-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
 
   # CIFAR-10(GAP): Square [Level: 7]
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L7_50-30 -D cifar-10 -M 5layer_cnn-square-BN-GAP --model-structure 5layer_cnn-square-BN-GAP-round1-58.88-structure.json --model-params 5layer_cnn-square-BN-GAP-round1-58.88-params.h5 -A square --fuse-layer --opt-pool --mode batch -N 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L7_50-30 -D cifar-10 -M 5layer_cnn-square-BN-GAP --model-structure 5layer_cnn-square-BN-GAP-round1-58.88-structure.json --model-params 5layer_cnn-square-BN-GAP-round1-58.88-params.h5 -A square --fuse-layer --opt-pool --mode batch -N 1)"
   # CIFAR-10(GAP): Swish approx. (rg5_deg2, rg7_deg4) [Level: 7 or 10]
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L7_50-30 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN-GAP --model-structure 5layer_cnn-swish_rg5_deg2-BN-GAP-round1-60.12-structure.json --model-params 5layer_cnn-swish_rg5_deg2-BN-GAP-round1-60.12-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L10_50-30 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN-GAP --model-structure 5layer_cnn-swish_rg7_deg4-BN-GAP-round1-60.83-structure.json --model-params 5layer_cnn-swish_rg7_deg4-BN-GAP-round1-60.83-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L7_50-30 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN-GAP --model-structure 5layer_cnn-swish_rg5_deg2-BN-GAP-round1-60.12-structure.json --model-params 5layer_cnn-swish_rg5_deg2-BN-GAP-round1-60.12-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L10_50-30 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN-GAP --model-structure 5layer_cnn-swish_rg7_deg4-BN-GAP-round1-60.83-structure.json --model-params 5layer_cnn-swish_rg7_deg4-BN-GAP-round1-60.83-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode batch -N 1)"
 
   ##############
   # Channel-wise
   ##############
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L7_50-30-60 -D cifar-10 -M 5layer_cnn-square-BN-GAP --model-params 5layer_cnn-square-BN-GAP-79.17_200epoch-0106_1855-params.h5 -A square --fuse-layer --opt-pool --mode single --images 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L7_50-30-60 -D cifar-10 -M 5layer_cnn-square-BN-GAP --model-params 5layer_cnn-square-BN-GAP-79.17_200epoch-0106_1855-params.h5 -A square --fuse-layer --opt-pool --mode single --images 1)"
   # CIFAR-10(GAP): Swish approx. (rg5_deg2, rg7_deg4) [Level: 7 or 10]
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L7_50-30-60 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN-GAP --model-params 5layer_cnn-swish_rg5_deg2-BN-GAP-79.53_200epoch-0107_0014-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode single --images 1)"
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L10_50-30-60 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN-GAP --model-params 5layer_cnn-swish_rg7_deg4-BN-GAP-80.91_200epoch-0107_0317-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode single --images 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L7_50-30-60 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN-GAP --model-params 5layer_cnn-swish_rg5_deg2-BN-GAP-79.53_200epoch-0107_0014-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode single --images 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L10_50-30-60 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN-GAP --model-params 5layer_cnn-swish_rg7_deg4-BN-GAP-80.91_200epoch-0107_0317-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode single --images 1)"
 
   ##############
   # Channel-wise (Pruned model)
   ##############
   # CIFAR-10(GAP): Square [Level: 7]
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L7_50-30-60 -D cifar-10 -M 5layer_cnn-square-BN-GAP --model-structure 5layer_cnn-square-BN-GAP-round1-58.88-structure.json --model-params 5layer_cnn-square-BN-GAP-round1-58.88-params.h5 -A square --fuse-layer --opt-pool --mode single --images 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L7_50-30-60 -D cifar-10 -M 5layer_cnn-square-BN-GAP --model-structure 5layer_cnn-square-BN-GAP-round1-58.88-structure.json --model-params 5layer_cnn-square-BN-GAP-round1-58.88-params.h5 -A square --fuse-layer --opt-pool --mode single --images 1)"
   # CIFAR-10(GAP): Swish approx. (rg5_deg2, rg7_deg4) [Level: 7 or 10]
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L7_50-30-60 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN-GAP --model-structure 5layer_cnn-swish_rg5_deg2-BN-GAP-round1-60.12-structure.json --model-params 5layer_cnn-swish_rg5_deg2-BN-GAP-round1-60.12-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode single --images 1)"
-  "(OMP_NUM_THREADS=72 /usr/bin/time -v ./bin/main -P N16384_L10_50-30-60 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN-GAP --model-structure 5layer_cnn-swish_rg7_deg4-BN-GAP-round1-60.83-structure.json --model-params 5layer_cnn-swish_rg7_deg4-BN-GAP-round1-60.83-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode single --images 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L7_50-30-60 -D cifar-10 -M 5layer_cnn-swish_rg5_deg2-BN-GAP --model-structure 5layer_cnn-swish_rg5_deg2-BN-GAP-round1-60.12-structure.json --model-params 5layer_cnn-swish_rg5_deg2-BN-GAP-round1-60.12-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --opt-pool --mode single --images 1)"
+  "(OMP_NUM_THREADS=72 ./bin/main -P N16384_L10_50-30-60 -D cifar-10 -M 5layer_cnn-swish_rg7_deg4-BN-GAP --model-structure 5layer_cnn-swish_rg7_deg4-BN-GAP-round1-60.83-structure.json --model-params 5layer_cnn-swish_rg7_deg4-BN-GAP-round1-60.83-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --opt-pool --mode single --images 1)"
 )
 
 if [ $# -ne 1 ]; then
--- mnist-calculate-homOps.sh
+++ mnist-calculate-homOps.sh
@@ -6,43 +6,43 @@ commands=(
   # Batch-axis
   ##############
   # MNIST: Square [Level: 5]
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-square-BN --model-params 3layer_cnn-square-BN-99.35_200epoch-1223_1032-params.h5 -A square --fuse-layer --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-square-BN --model-params 3layer_cnn-square-BN-99.35_200epoch-1223_1032-params.h5 -A square --fuse-layer --mode batch -N 1)"
   # MNIST: Swish approx. (rg5_deg2, rg7_deg4) [Level: 5 or 7]
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-params 3layer_cnn-swish_rg5_deg2-BN-99.48_200epoch-1223_2212-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode batch -N 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L7_50-30 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-params 3layer_cnn-swish_rg7_deg4-BN-99.52_200epoch-1224_0536-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-params 3layer_cnn-swish_rg5_deg2-BN-99.48_200epoch-1223_2212-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L7_50-30 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-params 3layer_cnn-swish_rg7_deg4-BN-99.52_200epoch-1224_0536-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode batch -N 1)"
 
   ##############
   # Batch-axis (Pruned model)
   ##############
   # MNIST: Square [Level: 5]
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-square-BN --model-structure 3layer_cnn-square-BN-round1-98.61-structure.json --model-params 3layer_cnn-square-BN-round1-98.61-params.h5 -A square --fuse-layer --mode batch -N 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-square-BN --model-structure 3layer_cnn-square-BN-round2-96.68-structure.json --model-params 3layer_cnn-square-BN-round2-96.68-params.h5 -A square --fuse-layer --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-square-BN --model-structure 3layer_cnn-square-BN-round1-98.61-structure.json --model-params 3layer_cnn-square-BN-round1-98.61-params.h5 -A square --fuse-layer --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-square-BN --model-structure 3layer_cnn-square-BN-round2-96.68-structure.json --model-params 3layer_cnn-square-BN-round2-96.68-params.h5 -A square --fuse-layer --mode batch -N 1)"
   # MNIST: Swish approx. (rg5_deg2, rg7_deg4) [Level: 5 or 7]
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-structure 3layer_cnn-swish_rg5_deg2-BN-round1-98.83-structure.json --model-params 3layer_cnn-swish_rg5_deg2-BN-round1-98.83-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode batch -N 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-structure 3layer_cnn-swish_rg5_deg2-BN-round2-95.52-structure.json --model-params 3layer_cnn-swish_rg5_deg2-BN-round2-95.52-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode batch -N 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L7_50-30 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-structure 3layer_cnn-swish_rg7_deg4-BN-round1-97.77-structure.json --model-params 3layer_cnn-swish_rg7_deg4-BN-round1-97.77-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode batch -N 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L7_50-30 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-structure 3layer_cnn-swish_rg7_deg4-BN-round2-96.78-structure.json --model-params 3layer_cnn-swish_rg7_deg4-BN-round2-96.78-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-structure 3layer_cnn-swish_rg5_deg2-BN-round1-98.83-structure.json --model-params 3layer_cnn-swish_rg5_deg2-BN-round1-98.83-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-structure 3layer_cnn-swish_rg5_deg2-BN-round2-95.52-structure.json --model-params 3layer_cnn-swish_rg5_deg2-BN-round2-95.52-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L7_50-30 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-structure 3layer_cnn-swish_rg7_deg4-BN-round1-97.77-structure.json --model-params 3layer_cnn-swish_rg7_deg4-BN-round1-97.77-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode batch -N 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L7_50-30 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-structure 3layer_cnn-swish_rg7_deg4-BN-round2-96.78-structure.json --model-params 3layer_cnn-swish_rg7_deg4-BN-round2-96.78-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode batch -N 1)"
 
   ##############
   # Channel-wise
   ##############
   # MNIST: Square [Level: 5]
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-square-BN --model-params 3layer_cnn-square-BN-99.35_200epoch-1223_1032-params.h5 -A square --fuse-layer --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-square-BN --model-params 3layer_cnn-square-BN-99.35_200epoch-1223_1032-params.h5 -A square --fuse-layer --mode single --images 1)"
   # MNIST: Swish approx. (rg5_deg2, rg7_deg4) [Level: 5 or 7]
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-params 3layer_cnn-swish_rg5_deg2-BN-99.48_200epoch-1223_2212-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode single --images 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L7_50-30-60 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-params 3layer_cnn-swish_rg7_deg4-BN-99.52_200epoch-1224_0536-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-params 3layer_cnn-swish_rg5_deg2-BN-99.48_200epoch-1223_2212-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L7_50-30-60 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-params 3layer_cnn-swish_rg7_deg4-BN-99.52_200epoch-1224_0536-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode single --images 1)"
 
   ##############
   # Channel-wise (Pruned model)
   ##############
   # MNIST: Square [Level: 5]
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-square-BN --model-structure 3layer_cnn-square-BN-round1-98.61-structure.json --model-params 3layer_cnn-square-BN-round1-98.61-params.h5 -A square --fuse-layer --mode single --images 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-square-BN --model-structure 3layer_cnn-square-BN-round2-96.68-structure.json --model-params 3layer_cnn-square-BN-round2-96.68-params.h5 -A square --fuse-layer --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-square-BN --model-structure 3layer_cnn-square-BN-round1-98.61-structure.json --model-params 3layer_cnn-square-BN-round1-98.61-params.h5 -A square --fuse-layer --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-square-BN --model-structure 3layer_cnn-square-BN-round2-96.68-structure.json --model-params 3layer_cnn-square-BN-round2-96.68-params.h5 -A square --fuse-layer --mode single --images 1)"
   # MNIST: Swish approx. (rg5_deg2, rg7_deg4) [Level: 5 or 7]
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-structure 3layer_cnn-swish_rg5_deg2-BN-round1-98.83-structure.json --model-params 3layer_cnn-swish_rg5_deg2-BN-round1-98.83-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode single --images 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-structure 3layer_cnn-swish_rg5_deg2-BN-round2-95.52-structure.json --model-params 3layer_cnn-swish_rg5_deg2-BN-round2-95.52-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode single --images 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L7_50-30-60 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-structure 3layer_cnn-swish_rg7_deg4-BN-round1-97.77-structure.json --model-params 3layer_cnn-swish_rg7_deg4-BN-round1-97.77-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode single --images 1)"
-  "(OMP_NUM_THREADS=24 /usr/bin/time -v ./bin/main -P N16384_L7_50-30-60 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-structure 3layer_cnn-swish_rg7_deg4-BN-round2-96.78-structure.json --model-params 3layer_cnn-swish_rg7_deg4-BN-round2-96.78-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-structure 3layer_cnn-swish_rg5_deg2-BN-round1-98.83-structure.json --model-params 3layer_cnn-swish_rg5_deg2-BN-round1-98.83-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L5_50-30-60 -D mnist -M 3layer_cnn-swish_rg5_deg2-BN --model-structure 3layer_cnn-swish_rg5_deg2-BN-round2-95.52-structure.json --model-params 3layer_cnn-swish_rg5_deg2-BN-round2-95.52-params.h5 -A swish_rg5_deg2 --fuse-layer --opt-act --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L7_50-30-60 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-structure 3layer_cnn-swish_rg7_deg4-BN-round1-97.77-structure.json --model-params 3layer_cnn-swish_rg7_deg4-BN-round1-97.77-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode single --images 1)"
+  "(OMP_NUM_THREADS=24 ./bin/main -P N16384_L7_50-30-60 -D mnist -M 3layer_cnn-swish_rg7_deg4-BN --model-structure 3layer_cnn-swish_rg7_deg4-BN-round2-96.78-structure.json --model-params 3layer_cnn-swish_rg7_deg4-BN-round2-96.78-params.h5 -A swish_rg7_deg4 --fuse-layer --opt-act --mode single --images 1)"
 )
 
 if [ $# -ne 1 ]; then
